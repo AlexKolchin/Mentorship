da0ea6a3c47a252a62997c5b29cf7f29c6fa54c9
okolc <akakolchin@gmail.com>
2020-11-13 12:00:24 +0200

    rows1 added func, ukrainian and 1-letter words

--- rows/main.py
+++ rows/main.py
@@ -1,16 +1,18 @@
 import re
+def repeat_letter(input_text):
 
-input_text = """ bbali taata - blabbla, tutu
-Tratata - blabla, tutu"""
-i = 0
-text = re.findall('[a-zа-яё]+', input_text, flags=re.IGNORECASE)
-new_list = []
-new_string = ''
-for word in text:
-    for i in range(len(word)):
-        if word[i] == word[i-1]:
-            new_list += [word]
-            input_text = input_text.replace(word, '')
-        i+=1
-print(input_text)
-print(' '.join(new_list))
+    i = 1
+    text = re.findall("[a-zа-яєїі']+", input_text, flags=re.IGNORECASE)
+    new_list = []
+    for word in text:
+        for i in range(len(word)):
+            if len(word) != 1:
+                if word[i-1] == word[i]:
+                    new_list += [word]
+                    input_text = input_text.replace(word, '')
+                i+=1
+    print(input_text)
+    print(' '.join(new_list))
+input_text = """ Пийшла осіння пора, ллє як з відра, п'є від сп'яніння!"""
+repeat_letter(input_text)
+
